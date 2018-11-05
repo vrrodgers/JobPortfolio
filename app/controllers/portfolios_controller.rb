735413b295a387b599a3b0d51eff1d73e8e8b000
vrrodgers <trulymissvicky1982@gmail.com>
2018-11-04 19:55:33 -0600

    added new and create for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,23 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+    
+    def new
+       @portfolio_item = Portfolio.new 
+    end
+    
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, 
+                                                                         :subtitle, 
+                                                                         :body
+                                                                        ))
+
+       
+        if @portfolio_item.save
+             redirect_to @portfolios_path, notice: 'Portfolio was successfully created.'
+        else
+            render :new
+        end
+       
+    end
 end
