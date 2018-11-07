6dc3be72d53639f49d50dfd775c03c62e580b758
vrrodgers <trulymissvicky1982@gmail.com>
2018-11-06 19:30:50 -0600

    implemented custom scopes for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+3.times do |topic|
+    Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+puts "3 Topics created"
+
 10.times do |blog|
     Blog.create!(
         title: "My blog Post #{blog}",
@@ -9,7 +16,8 @@
          it to make a type specimen book. It has 
          survived not only five centuries, but also 
          the leap into electronic typesetting, 
-         remaining essentially unchanged. It was"
+         remaining essentially unchanged. It was,",
+         topic_id: Topic.last.id
     )
 end
 puts "10 blog posts created"
@@ -21,16 +29,24 @@ puts "10 blog posts created"
     )
 end
 puts "5 skills created" 
-
-9.times do |portfolio_item|
-    Portfolio.create!(
-        title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My great service",
-        body: "It is a long established fact that a reader will 
-          be distracted by the readable content of a page when looking at
-          its layout. The point of using Lorem Ipsum is that it has a 
-          more-or-less normal distribution of letters, as opposed to using",
-         main_image: "https://via.placeholder.com/150",
-        thumb_image: "https://via.placeholder.com/350"
+8.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
 end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
 puts "9 portfolio items created"
