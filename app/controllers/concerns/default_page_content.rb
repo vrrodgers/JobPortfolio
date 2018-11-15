32dc0e18d04591e0f501c0d0d773d23bfeab0f03
vrrodgers <trulymissvicky1982@gmail.com>
2018-11-14 18:32:49 -0600

    added customs layouts for blogs and portfolios

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,14 @@
+module DefaultPageContent
+    extend ActiveSupport::Concern
+
+    included do 
+       before_action :set_page_defaults
+    end
+
+    def set_page_defaults
+      @page_title = " My Portfolio Website"
+      @seo_keywords = "Ruby on Rails"
+
+    end
+
+end
