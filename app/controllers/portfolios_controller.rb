407955acf88780fbb002420e2da4dfd05444d342
vrrodgers <trulymissvicky1982@gmail.com>
2018-12-06 18:45:59 -0600

    implemented initial file uploading system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -74,6 +74,9 @@ class PortfoliosController < ApplicationController
         params.require(:portfolio).permit(:title,
                                          :subtitle, 
                                          :body,
-                                        technologies_attributes: [:name])
+                                         :main_image,
+                                        :thumb_image,
+                                        technologies_attributes: [:name]
+                                    )
     end
 end
