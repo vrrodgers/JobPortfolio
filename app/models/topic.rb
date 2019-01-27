afe5e3de384e61c57779a8b36796a7a050969ba1
vrrodgers <trulymissvicky1982@gmail.com>
2019-01-27 16:32:29 -0600

    fix blog plublished issue

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,10 @@
 class Topic < ApplicationRecord
     validates_presence_of :title
     has_many :blogs
-
+    
+    def self.with_blogs_published
+      includes(:blogs).where.not(blogs: {id: nil, status: "draft"})
+    end
     def self.with_blogs
         includes(:blogs).where.not(blogs: {id: nil})
     end
